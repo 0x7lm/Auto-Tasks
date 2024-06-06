d87d642a9d95ec5b0470ae0bc27cf8b86bfcd8b0
audit4me <randaljohnseo@gmail.com>
2024-06-06 17:13:55 +0300

    adding some test fokred spolia v0.1

--- test/AutoTasksWithSub.t.sol
+++ test/AutoTasksWithSub.t.sol
@@ -8,7 +8,6 @@ import { PegSwap } from "../src/PegSwap.sol";
 import { RegisterUpkeep } from "../src/RegisterUpKeep.sol";
 import { Check } from "../src/chack.sol";
 import { IERC20 } from "../src/interface/IERC20.sol";
-import {IUSDC }  from "../src/interface/IUSDC.sol";
 import { IWETH } from "../src/interface/IWETH.sol";
 
 contract AutoTasksWithSubTest is StdInvariant, Test {
@@ -21,7 +20,8 @@ contract AutoTasksWithSubTest is StdInvariant, Test {
     Check check;
     
     IWETH private weth = IWETH(0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14);
-    IUSDC private usdc = IUSDC(0x51fCe89b9f6D4c530698f181167043e1bB4abf89);
+    //IUSDC private usdc = IUSDC(0x51fCe89b9f6D4c530698f181167043e1bB4abf89);
+    IERC20 private usdc = IERC20(0x51fCe89b9f6D4c530698f181167043e1bB4abf89);
     IERC20 private link = IERC20(0x779877A7B0D9E8603169DdbD7836e478b4624789);
 
     address private constant UNISWAP_V2_ROUTER = 0x86dcd3293C53Cf8EFd7303B57beb2a3F671dDE98;
@@ -33,18 +33,28 @@ contract AutoTasksWithSubTest is StdInvariant, Test {
     string[] public args = ["0x1234567890abcdef1234567890abcdef12345678", "2"];
     uint256 constant interval = 3600;
 
+
+    /**
+     * 1- First mint user 1000 usdc 
+     * 2- Aprove the swap contract the amount 
+     * 3- then it just creating Automation 
+     */
+
     function setUp() public {
         check = new Check();
         registerUpkeep = new RegisterUpkeep();
         pegSwap = new PegSwap(autoTasks, registerUpkeep, check);
         autoTasks = new AutoTasksWithSub(pegSwap, check, registerUpkeep);
-
+        
+        vm.deal(address(this), 10 ether);
         vm.prank(usdc.masterMinter());
         // allow this test user to mint USDC
         usdc.configureMinter(address(this), type(uint256).max);
         
         // mint $1000 USDC to the test user
         usdc.mint(address(user), 1000e6);
+        //usdc.mint(address(pegSwap),40e6);
+        // usdc.approve(address(pegSwap),40e6);
     }
 
     function testBalance() public {
@@ -58,19 +68,21 @@ contract AutoTasksWithSubTest is StdInvariant, Test {
         // Swap USDC -> ETH -> LINK
         uint256 usdcAmountIn = 40e6;
         usdc.approve(address(pegSwap), usdcAmountIn);
-        uint256 linkAmountOutMin = 2;     // 2 link token
+        uint256 pegSwapBalance = usdc.balanceOf(address(pegSwap));
+        uint256 linkAmountOutMin = 2e18;     // 2 link token
         uint256 ethAmountOutMin = 10e14;  // 5 usd
         uint256 linkShare = 25;           // 25% of 30 USDC
         uint256 ethShare = 5;             // 5% of 30 USDC
-        pegSwap.swapAndFund{ value: ethAmountOutMin }(usdcAmountIn, linkAmountOutMin, ethAmountOutMin, linkShare, ethShare);
+        //pegSwap.swapAndFund(usdcAmountIn, linkAmountOutMin, ethAmountOutMin, linkShare, ethShare);
         
         testCreateAutomation();
+        assertEq(pegSwapBalance, usdcAmountIn);
         vm.stopPrank();
     }    
 
     function testCreateAutomation() public {
         vm.startPrank(user);
-        bool success = autoTasks.createAutomation{value: 35e8 }(
+        bool success = autoTasks.createAutomation{value: 40e6}(
             contractToAutomate,
             upkeepName,
             fnSignature,
